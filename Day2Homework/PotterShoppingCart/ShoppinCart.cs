fe4e6888ef767b1846338e136fbca5c5f3a254dc
Roger Chang <Roger Chang@DESKTOP-0BRU83S>
2016-10-22 17:54:32 +0800

    通過第六個測試案例

--- Day2Homework/PotterShoppingCart/ShoppinCart.cs
+++ Day2Homework/PotterShoppingCart/ShoppinCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PotterShoppingCart
 {
@@ -18,18 +19,36 @@ namespace PotterShoppingCart
 
         private double GetTotalPrice(List<HarryPotter> books)
         {
-            switch(books.Count)
+            double totalPrice = 0;
+            int index = 0;
+            int booksCates = books.GroupBy(x => x.Episode).Count();
+            while (booksCates != 0)
+            {
+                booksCates = books.GroupBy(x => x.Episode)
+                    .Select(y => y.Skip(index).FirstOrDefault())
+                    .Where(z => z != null).Count();
+
+                totalPrice += GetDiscount(booksCates);
+                index++;
+            }
+
+            return totalPrice;
+        }
+
+        private static double GetDiscount(int booksCates)
+        {
+            switch (booksCates)
             {
                 case 2:
-                    return 100 * books.Count * 0.95;
+                    return 100 * booksCates * 0.95;
                 case 3:
-                    return 100 * books.Count * 0.9;
+                    return 100 * booksCates * 0.9;
                 case 4:
-                    return 100 * books.Count * 0.8;
+                    return 100 * booksCates * 0.8;
                 case 5:
-                    return 100 * books.Count * 0.75;
+                    return 100 * booksCates * 0.75;
                 default:
-                    return 100 * books.Count;
+                    return 100 * booksCates;
             }
         }
     }
