ce0372905fd2e25433f653b0dfac7b347e1c07a2
Roger Chang <Roger Chang@DESKTOP-0BRU83S>
2016-10-22 16:39:39 +0800

    第二個測試案例紅燈

--- Day2Homework/PotterShoppingCartTests/PotterShoppingCartTest.cs
+++ Day2Homework/PotterShoppingCartTests/PotterShoppingCartTest.cs
@@ -26,5 +26,25 @@ namespace PotterShoppingCartTests
             double expected = 100;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Buy_1_EP_And_1_EP2_Total_Should_Be_190()
+        {
+            //arrange
+            ShoppinCart target = new ShoppinCart();
+            List<HarryPotter> books = new List<HarryPotter>
+            {
+                new HarryPotter { Episode = "1" },
+                new HarryPotter { Episode = "2" }
+            };
+
+            //act
+            target.AddToCart(books);
+            double actual = target.TotalPrice;
+
+            //assert
+            double expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
